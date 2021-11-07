6b46c5dcfa32ee01995bf0facff3f35986f4d097
murdr <superepicfalcon@gmail.com>
2021-11-07 21:30:48 +0700

    Login Response Adjustment & Loader

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import {IonButton, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonRouterLink, IonRow, IonTitle, IonToolbar, useIonToast } from '@ionic/react';
+import {IonButton, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonRouterLink, IonRow, IonTitle, IonToolbar, useIonLoading, useIonToast } from '@ionic/react';
 import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router';
 import { urlLogin } from '../data/Urls';
@@ -17,6 +17,7 @@ const Login: React.FC = () => {
   //   history.push('/wallet')
   // }
 
+  const [showLoader, hideLoader] = useIonLoading()
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
@@ -41,27 +42,32 @@ const Login: React.FC = () => {
     const formData = new FormData()
     formData.append('email',email)
     formData.append('password',password)
-
+    
+    showLoader({
+      message: "Loading...",
+      spinner: "circular"
+    })
     fetch(urlLogin,{ 
       method: "POST",
       body: formData
     }).then(res => res.json())
     .then(data => {
       console.log(data)
+      hideLoader()
 
       // Sukses login
       if(data.message == "Login success"){
         showToast('Login success','success')
 
         // Simpan token
-        userContext.storeToken(data.token)
+        userContext.storeToken(data.data.token)
 
         // TODO: Redirect to dashboard
         history.push('/wallet')
       }
       // Gagal login
       else{
-        showToast(data.message,'danger')
+        showToast(data.errors.message,'danger')
       }
     })
   }
@@ -109,7 +115,7 @@ const Login: React.FC = () => {
 
             <IonRow className="mt-4">
               <IonCol className="ion-text-center">
-                <IonButton expand="block" className="button-login" strong={true} type="submit" onClick={submitLoginHandler}>Submit</IonButton>
+                <IonButton expand="block" className="button-login" strong={true} type="submit" onClick={submitLoginHandler}>Login</IonButton>
                 <div className="mt-3">
                   <p>Doesn't have account? <IonRouterLink routerLink="/register">Register Now</IonRouterLink></p>
                 </div>
