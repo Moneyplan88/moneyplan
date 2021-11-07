81b5f50f00f62fe70ef6e01a0a26b4122d5d1abb
Daniel W <danielwj00@gmail.com>
2021-11-07 10:44:30 +0800

    Get user data

--- src/data/UserContextProvider.tsx
+++ src/data/UserContextProvider.tsx
@@ -1,9 +1,13 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import UserContext from './user-context'
 import { Storage } from '@capacitor/storage';
+import axios from 'axios'
+import { urlUserInfo } from './Urls';
+import UserModel from '../model/user.model';
 
 const UserContextProvider: React.FC = props => {
     const [token, setToken] = useState('')
+    const [user, setUser] = useState({})
 
     const storeToken = (token: string) => {
         console.log(token)
@@ -15,6 +19,15 @@ const UserContextProvider: React.FC = props => {
         const tokenRes = result.value ? JSON.parse(result.value) : ''
         console.info("initing ... ",tokenRes)
         setToken(tokenRes)
+
+        const resUserInfo = (await axios.get(urlUserInfo, {
+            headers: {
+                'Authorization': `Bearer ${tokenRes}`
+            }
+        })).data;
+        const userInfo:UserModel = resUserInfo.data;
+        setUser(userInfo)
+        console.info(userInfo)
     },[])
 
     useEffect(() => {
@@ -22,7 +35,7 @@ const UserContextProvider: React.FC = props => {
     }, [token])
 
     return (
-        <UserContext.Provider value={{token, storeToken, initContext}}>
+        <UserContext.Provider value={{token, user, storeToken, initContext}}>
             {props.children}
         </UserContext.Provider>
     )
--- src/data/user-context.ts
+++ src/data/user-context.ts
@@ -1,17 +1,14 @@
 import React from 'react'
-
-export interface User{
-    name: string;
-    email: string;
-    token: string;
-}
+import UserModel from '../model/user.model'
 
 const UserContext = React.createContext<{
     token: string;
+    user: UserModel
     storeToken: (token: string) => void;
     initContext: () => void,
 }>({
     token: '',
+    user: {},
     storeToken: () => {},
     initContext: () => {}
 })
