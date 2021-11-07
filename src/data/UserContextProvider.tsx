6626de45da85ca2130b2af2537fa323abaebf759
Daniel W <danielwj00@gmail.com>
2021-11-07 18:22:24 +0800

    null check fix

--- src/data/UserContextProvider.tsx
+++ src/data/UserContextProvider.tsx
@@ -1,44 +1,48 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import UserContext from './user-context'
-import { Storage } from '@capacitor/storage';
+import { Storage } from '@capacitor/storage'
 import axios from 'axios'
-import { urlUserInfo } from './Urls';
-import UserModel from '../model/user.model';
+import { urlUserInfo } from './Urls'
+import UserModel from '../model/user.model'
 
-const UserContextProvider: React.FC = props => {
-    const [token, setToken] = useState('')
-    const [user, setUser] = useState({})
+const UserContextProvider: React.FC = (props) => {
+  const [token, setToken] = useState('')
+  const [user, setUser] = useState({})
 
-    const storeToken = (token: string) => {
-        console.log(token)
-        setToken(token)
+  const storeToken = (token: string) => {
+    console.log(token)
+    setToken(token)
+  }
+
+  const initContext = useCallback(async () => {
+    const result = await Storage.get({ key: 'token' })
+    const tokenRes = result.value ? JSON.parse(result.value) : ''
+    console.info('initing ... ', tokenRes)
+    setToken(tokenRes)
+
+    if (result.value) {
+      const resUserInfo = (
+        await axios.get(urlUserInfo, {
+          headers: {
+            Authorization: `Bearer ${tokenRes}`,
+          },
+        })
+      ).data
+      const userInfo: UserModel = resUserInfo.data
+      setUser(userInfo)
+      console.info(userInfo)
     }
+  }, [])
 
-    const initContext = useCallback( async () => {
-        const result = await Storage.get({key: 'token'})
-        const tokenRes = result.value ? JSON.parse(result.value) : ''
-        console.info("initing ... ",tokenRes)
-        setToken(tokenRes)
+  useEffect(() => {
+    Storage.set({ key: 'token', value: JSON.stringify(token) })
+  }, [token])
 
-        const resUserInfo = (await axios.get(urlUserInfo, {
-            headers: {
-                'Authorization': `Bearer ${tokenRes}`
-            }
-        })).data;
-        const userInfo:UserModel = resUserInfo.data;
-        setUser(userInfo)
-        console.info(userInfo)
-    },[])
-
-    useEffect(() => {
-        Storage.set({key: 'token',value: JSON.stringify(token)})
-    }, [token])
-
-    return (
-        <UserContext.Provider value={{token, user, storeToken, initContext}}>
-            {props.children}
-        </UserContext.Provider>
-    )
+  return (
+    <UserContext.Provider value={{ token, user, storeToken, initContext }}>
+      {props.children}
+    </UserContext.Provider>
+  )
 }
 
 export default UserContextProvider
